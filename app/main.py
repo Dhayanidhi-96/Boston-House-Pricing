1c4e80c4941e78817dbf1d83b44017d5ad37582a
Dhayanidhi-96 <dhayanidhi962004@gmail.com>
2025-12-27 16:05:41 +0530

    Prepare FastAPI app for deployment on Render

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from app.routes import router
 
 app = FastAPI(
-    title= "Boston House Price Prediction API",
-    version = "1.0.0"
+    title="Boston House Price Prediction",
+    version="1.0"
 )
 
+# Load templates
+templates = Jinja2Templates(directory="templates")
+
+# API routes
 app.include_router(router)
 
-@app.get("/")
-def home():
-    return {"message": "API is running sucessfully"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "prediction": None}
+    )
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,59 @@
-from fastapi import APIRouter
-from app.schemas import HouseInput
+from fastapi import APIRouter, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from src.predict import predict_price
 
-router  = APIRouter()
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
-@router.post("/predict")
-def predict_house_price(data: HouseInput):
-    price = predict_price(data.dict())
-    return{
-        "status": "success",
-        "predicted_price" : price
+
+@router.post("/", response_class=HTMLResponse)
+def predict_ui(
+    request: Request,
+    CRIM: float = Form(...),
+    ZN: float = Form(...),
+    INDUS: float = Form(...),
+    CHAS: int = Form(...),
+    NOX: float = Form(...),
+    RM: float = Form(...),
+    AGE: float = Form(...),
+    DIS: float = Form(...),
+    RAD: int = Form(...),
+    TAX: float = Form(...),
+    PTRATIO: float = Form(...),
+    B: float = Form(...),
+    LSTAT: float = Form(...)
+):
+    input_data = {
+        "CRIM": CRIM,
+        "ZN": ZN,
+        "INDUS": INDUS,
+        "CHAS": CHAS,
+        "NOX": NOX,
+        "RM": RM,
+        "AGE": AGE,
+        "DIS": DIS,
+        "RAD": RAD,
+        "TAX": TAX,
+        "PTRATIO": PTRATIO,
+        "B": B,
+        "LSTAT": LSTAT,
     }
+
+    prediction = predict_price(input_data)
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "prediction": prediction
+        }
+    )
+
+
+# API endpoint still available
+@router.post("/predict")
+def predict_api(data: dict):
+    price = predict_price(data)
+    return {"predicted_price": price}
